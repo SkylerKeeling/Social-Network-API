6ed8f54d744d1f81eee91188dcdf6a7608dc9761
skyler <skyler@skylers-MacBook-Pro.local>
2023-09-27 12:08:54 -0300

    thought controller added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 const {User} = require("../models")
 
 module.exports = {
-  async getUsers(req, res) {
+  async getAllUsers(req, res) {
     try {
       const users = await User.find()
       res.json(users)

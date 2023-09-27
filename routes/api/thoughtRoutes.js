ca79df54bc2991f6f8d8677f9c5589cb46f8a00e
skyler <skyler@skylers-MacBook-Pro.local>
2023-09-27 12:16:06 -0300

    thought and user routes completed

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router()
+const {
+  getAllThoughts,
+  getOneThought,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require("../../controllers/thoughtController.js")
+
+router.route("/").get(getAllThoughts).post(createThought)
+
+router
+  .route("/:thoughtId")
+  .get(getOneThought)
+  .put(updateThought)
+  .delete(deleteThought)
+
+module.exports = router
